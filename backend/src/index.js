7cae3744fd717ec17357550283c8e83934e74986
Peterovsky <Peterovsky7@gmail.com>
2019-08-04 23:55:56 +0200

    Set up connection to the Postgres database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,13 @@
 require('dotenv').config({ path: '.env' });
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const db = require('../config/database.js');
+
+// Test database connection
+db.authenticate()
+    .then(() => console.log('Database connected'))
+    .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
     res.json({ status: "ON" })
