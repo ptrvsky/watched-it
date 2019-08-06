23a09f317c87fbdccbe927c74bcc2e2484ddf719
Peterovsky <Peterovsky7@gmail.com>
2019-08-06 18:04:45 +0200

    Add production-person assignment model

---
+++ backend/models/productionPerson.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database.js');
+
+// Stores information about assignments between productions and people
+const ProductionPerson = db.define('production_person', {
+    personId: {
+        type: Sequelize.INTEGER
+    },
+    productionId: {
+        type: Sequelize.INTEGER
+    },
+    role: {
+        type: Sequelize.ENUM('Actor', 'Writer', 'Director', 'Composer', 'Producer')
+    }
+},{
+    tableName: 'productions_people'
+});
+
+module.exports = ProductionPerson;
