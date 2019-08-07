5b6c4644c51cc45413719f3ef25383e077b14971
Peterovsky <Peterovsky7@gmail.com>
2019-08-08 01:26:44 +0200

    Fix production length schema

--- backend/src/schemas/production.js
+++ backend/src/schemas/production.js
@@ -2,7 +2,7 @@ const Joi = require('joi');
 
 const schema = Joi.object().keys({
     title: Joi.string().min(2).max(100),
-    length: Joi.number().min(1).max(999999),
+    length: Joi.number().integer().min(1).max(999999),
     releaseDate: Joi.date().min('1-1-1800'),
     isSerie: Joi.boolean().default(false)
 });
