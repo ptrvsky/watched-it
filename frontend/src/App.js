50db3bd4d064d7a0b460ce0a21853a305c5482c1
Peterovsky <Peterovsky7@gmail.com>
2019-08-26 15:15:41 +0200

    Set up frontend with Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <i data-feather="monitor"></i>
+        </p>
+          Watched It
+      </header>
+    </div>
+  );
+}
+
+export default App;
