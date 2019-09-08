f4e60f8c4c970a2389a531b6d8b13b5ff41ee077
Peterovsky <Peterovsky7@gmail.com>
2019-09-08 18:20:17 +0200

    Fix and improve movies subpage

--- frontend/src/components/MoviesSubpage/MovieDetailsMedium/MovieDetailsMedium.js
+++ frontend/src/components/MoviesSubpage/MovieDetailsMedium/MovieDetailsMedium.js
@@ -7,30 +7,42 @@ export default class MovieDetailsMedium extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      poster: null
+      poster: null,
+      director: []
     }
   }
 
   componentDidMount() {
     fetch('/api/images/' + this.props.movie.posterId)
       .then(poster => poster.json())
-      .then(poster => this.setState({ poster }));
+      .then(poster => {
+        this.setState({ poster: poster.url })
+      });
+
+    fetch('/api/productions/' + this.props.movie.id + '/people')
+      .then(people => people.json())
+      .then(people => people.filter((person) => person.role === "Director"))
+      .then(directors => directors.map(director => {
+        fetch('/api/people/' + director.personId)
+          .then(director => director.json())
+          .then(director => this.setState({ director: this.state.director.concat(director.name) }))
+      }))
   }
 
   render() {
     return (
       <div className="movie-details-medium-wrapper">
-        <div className="poster"><img src={"/api/" + this.state.posterURL} alt="poster" /></div>
+        <div className="poster">{<img src={"/api/" + this.state.poster} alt="poster" />}</div>
         <div className="eye-button"><Eye size={28} /></div>
         <div className="info">
           <div className="title">{this.props.movie.title} ({this.props.movie.releaseDate.slice(0, 4)})</div>
           <div className="rating">
-            <Star className="star" size={20} /> <div className="text">  </div><div className="votes-amount">  </div>
+            <Star className="star" size={20} /> <div className="text"> </div><div className="votes-amount"> </div>
           </div>
           <div className="details">
             <div><span className="category">Genre:</span> {this.props.movie.genre.join(", ")}</div>
             <div><span className="category">Length: </span> {parseInt(this.props.movie.length / 60)}h {(this.props.movie.length % 60 !== 0) ? this.props.movie.length % 60 + "min" : null}</div>
-            <div><span className="category">Director:</span>  </div>
+            <div className="director"><span className="category">Director:</span> {this.state.director.join(", ")}</div>
           </div>
         </div>
       </div>
