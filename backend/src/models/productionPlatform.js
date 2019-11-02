b887907f18cd2b131aa6df41c3655f27c663bc3d
Peterovsky <Peterovsky7@gmail.com>
2019-11-02 23:55:18 +0100

    Add production-platform model and schema

---
+++ backend/src/models/productionPlatform.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database.js');
+
+// Stores information about assignments between productions and platforms
+const ProductionPlatform = db.define('production_platform', {
+    productionId: {
+        type: Sequelize.INTEGER,
+    },
+    platformId: {
+        type: Sequelize.INTEGER,
+    },
+}, {
+    tableName: 'productions_platforms',
+});
+
+module.exports = ProductionPlatform;
---
+++ backend/src/schemas/productionPlatform.js
@@ -0,0 +1,8 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    productionId: Joi.number().integer().min(1),
+    platformId: Joi.number().integer().min(1),
+});
+
+module.exports = schema;
