31d095db79cfff5cc2ba2629a61caf2421541baf
Peterovsky <Peterovsky7@gmail.com>
2019-11-03 19:04:31 +0100

    Add platform model and schema

---
+++ backend/src/models/platform.js
@@ -0,0 +1,11 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database.js');
+
+// Stores information about platforms that contains productions
+const Platform = db.define('platform', {
+    name: {
+        type: Sequelize.STRING,
+    },
+});
+
+module.exports = Platform;
---
+++ backend/src/schemas/platform.js
@@ -0,0 +1,7 @@
+const Joi = require('joi');
+
+const schema = Joi.object().keys({
+    name: Joi.string().min(1).max(40),
+});
+
+module.exports = schema;
