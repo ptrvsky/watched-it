46b8258a70491d2cd573903c12051490c349b256
Peterovsky <Peterovsky7@gmail.com>
2019-12-06 21:36:43 +0100

    Add user verification to methods that return watchlist

--- backend/src/controllers/userProduction.js
+++ backend/src/controllers/userProduction.js
@@ -6,32 +6,46 @@ UserProduction.belongsTo(Production);
 
 // Get user-production assignments of selected user
 exports.getUserProductionAssignmentsByUser = (req, res, next) => {
-  UserProduction.findAll({
-    where: {
-      userId: req.params.userId,
-    },
-    include: Production,
-    limit: req.query.limit,
-    offset: req.query.offset,
-  })
-    .then((usersProductionsAssignments) => {
-      res.status(200).json(usersProductionsAssignments);
-    })
-    .catch(next);
+  if (req.user) {
+    // eslint-disable-next-line eqeqeq
+    if (req.user.id == req.params.userId) {
+      UserProduction.findAll({
+        where: {
+          userId: req.params.userId,
+        },
+        include: Production,
+        limit: req.query.limit,
+        offset: req.query.offset,
+      })
+        .then((usersProductionsAssignments) => {
+          res.status(200).json(usersProductionsAssignments);
+        })
+        .catch(next);
+    }
+  } else {
+    res.status(401).end();
+  }
 };
 
 // Get assignment between production and user with given ids
 exports.getUserProductionAssignmentByIds = (req, res, next) => {
-  UserProduction.findOne({
-    where: {
-      userId: req.params.userId,
-      productionId: req.params.productionId,
-    },
-  })
-    .then((userProductionAssignment) => {
-      res.status(200).json(userProductionAssignment);
-    })
-    .catch(next);
+  if (req.user) {
+    // eslint-disable-next-line eqeqeq
+    if (req.user.id == req.params.userId) {
+      UserProduction.findOne({
+        where: {
+          userId: req.params.userId,
+          productionId: req.params.productionId,
+        },
+      })
+        .then((userProductionAssignment) => {
+          res.status(200).json(userProductionAssignment);
+        })
+        .catch(next);
+    }
+  } else {
+    res.status(401).end();
+  }
 };
 
 // Create user-production assignment
