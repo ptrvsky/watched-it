ab192a3681111fff752933e5a8286b65321a7de9
Peterovsky <Peterovsky7@gmail.com>
2019-12-30 13:46:50 +0100

    Add productions fetching based on user input

--- frontend/src/components/Navbar/SearchBar/SearchBar.js
+++ frontend/src/components/Navbar/SearchBar/SearchBar.js
@@ -7,13 +7,31 @@ export default class SearchBar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      result: null,
+    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    if (event.target.value) {
+      fetch('/api/productions?limit=3&search=' + event.target.value)
+        .then(response => response.json())
+        .then(response => this.setState({
+          result: response.rows,
+        }));
+    } else {
+      this.setState({
+        result: null,
+      })
+    }
   }
 
   render() {
     return (
       <div className={this.props.isMobile ? "search-bar" : "search-bar search-bar--desktop"} >
         <Form inline >
-          <input className="search-input" type="text" placeholder="Search" />
+          <input className="search-input" type="text" placeholder="Search" onInput={this.handleInputChange} />
           <button className="btn"><Search /></button>
         </Form>
         <div className="search-results"></div>
