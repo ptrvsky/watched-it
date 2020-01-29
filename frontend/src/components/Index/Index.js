2dfba9457e336fca68b043db7e323cb7b81c8bd1
Peterovsky <Peterovsky7@gmail.com>
2020-01-29 20:30:59 +0100

    Fix "Sign up" button on index page

--- frontend/src/components/Index/Index.js
+++ frontend/src/components/Index/Index.js
@@ -3,16 +3,38 @@ import { Link } from "react-router-dom";
 import '../../assets/styles/buttons.scss';
 import './Index.scss';
 
-function Index() {
-  return (
-    <div className="index-wrapper">
-      <div className="p1">Complete TV watching assistant.</div>
-      <div className="p2">Track, rate, discover.</div>
-      <Link to="/register">
-        <button className="btn-primary">Sign up</button>
-      </Link>
-    </div>
-  );
-}
+export default class Index extends React.Component {
 
-export default Index;
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        status: 'NOT_LOGGED',
+      },
+    }
+  }
+
+  componentDidMount() {
+    fetch('/api/users/auth')
+      .then((user) => user.json())
+      .then((user) => this.setState({ user }));
+  }
+
+  render() {
+    return (
+      <div className="index-wrapper">
+        <div className="p1">Complete TV watching assistant.</div>
+        <div className="p2">Track, rate, discover.</div>
+        {this.state.user.status === 'LOGGED' ?
+          <Link to="/movies">
+            <button className="btn-primary">Browse</button>
+          </Link>
+          :
+          <Link to="/register">
+            <button className="btn-primary">Sign up</button>
+          </Link>
+        }
+      </div>
+    );
+  }
+}
